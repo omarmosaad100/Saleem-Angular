4ca016b7d8c3adb451f80e0462738dff0d44f855
shahendahamdy <shahendahamdy666@gmail.com>
2023-04-22 02:36:00 +0200

    view patient History based on national ID

---
+++ src/app/Components/view-patient-history/view-patient-history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-patient-history',
+  templateUrl: './view-patient-history.component.html',
+  styleUrls: ['./view-patient-history.component.css']
+})
+export class ViewPatientHistoryComponent {
+
+}
