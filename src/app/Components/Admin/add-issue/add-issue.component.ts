f4d412ab6b867840585456634879e9cb8c3d4c3f
Youssef Ehab <82692312+Youssef-Ehab77@users.noreply.github.com>
2023-04-23 23:21:29 +0200

    Add Issue

--- src/app/Components/Admin/add-issue/add-issue.component.ts
+++ src/app/Components/Admin/add-issue/add-issue.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AdminService} from "../../../Services/AdminService/admin.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add-issue',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-issue.component.css']
 })
 export class AddIssueComponent {
+  IssueName: string = "";
 
+  constructor(private myService: AdminService, private router: Router) {
+  }
+
+  addIssue() {
+    this.myService.AddNewIssue(this.IssueName).subscribe({
+      next: (data) => {
+      },
+      error: (error) => {
+        alert(error.message);
+        //alert("Make Sure that the Data is correct!")
+      },
+      complete: () => {
+        alert("Issue Added Successfully!")
+      }
+    });
+  }
 }
--- src/app/Services/AdminService/admin.service.ts
+++ src/app/Services/AdminService/admin.service.ts
@@ -1,45 +1,53 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor(private readonly myClient:HttpClient) {}
+  constructor(private readonly myClient: HttpClient) {
+  }
 
   private readonly URL = "https://localhost:7016/api/Admin";
 
-  getAllDrugs(){
-    return this.myClient.get(this.URL+"/GetAllDrugs");
-  }
-  getAllDoctors(){
-    return this.myClient.get(this.URL+"/GetAllDoctors");
-  }
-  getDrugById(id:any){
-    return this.myClient.get(this.URL+"/GetDrug/"+id);
-  }
-  AddNewDrug(Drug:any)
-  {
-    return this.myClient.post(this.URL+"/AddDrug" , Drug);
-  }
-  UpdateDrugById(id:number , Drug:any)
-  {
-    return this.myClient.put(this.URL+"/UpdateDrug/"+id , Drug);
-  }
-  DeleteDrugById(id:any)
-  {
-    return this.myClient.delete(this.URL+"/DeleteDrug/"+id);
-  }
-  DeleteDoctorById(id:any){
-    return this.myClient.delete(this.URL+"/RemoveDoctorLicense?id="+id);
+  getAllDrugs() {
+    return this.myClient.get(this.URL + "/GetAllDrugs");
   }
 
-  AddNewDoctorLicense(nationalId:any){
-    return this.myClient.post(this.URL+"/AddLicense", nationalId);
+  getAllDoctors() {
+    return this.myClient.get(this.URL + "/GetAllDoctors");
   }
 
-  getAllIssues(){
-    return this.myClient.get(this.URL+"/GetAllIssues");
+  getDrugById(id: any) {
+    return this.myClient.get(this.URL + "/GetDrug/" + id);
+  }
+
+  AddNewDrug(Drug: any) {
+    return this.myClient.post(this.URL + "/AddDrug", Drug);
+  }
+
+  UpdateDrugById(id: number, Drug: any) {
+    return this.myClient.put(this.URL + "/UpdateDrug/" + id, Drug);
+  }
+
+  DeleteDrugById(id: any) {
+    return this.myClient.delete(this.URL + "/DeleteDrug/" + id);
+  }
+
+  DeleteDoctorById(id: any) {
+    return this.myClient.delete(this.URL + "/RemoveDoctorLicense?id=" + id);
+  }
+
+  AddNewDoctorLicense(nationalId: any) {
+    return this.myClient.post(this.URL + "/AddLicense", nationalId);
+  }
+
+  getAllIssues() {
+    return this.myClient.get(this.URL + "/GetAllIssues");
+  }
+
+  AddNewIssue(IssueName: string) {
+    return this.myClient.post(this.URL + "/AddIssue", {Name:IssueName})
   }
 }
