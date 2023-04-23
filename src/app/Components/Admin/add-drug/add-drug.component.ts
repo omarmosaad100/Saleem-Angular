a0fd94d98d61d9d4b6c962f9648de83d03728565
Youssef Ehab <82692312+Youssef-Ehab77@users.noreply.github.com>
2023-04-23 21:31:05 +0200

    add new drug now works

--- src/app/Components/Admin/add-drug/add-drug.component.ts
+++ src/app/Components/Admin/add-drug/add-drug.component.ts
@@ -13,19 +13,14 @@ import {DrugDto} from './drugDto';
   styleUrls: ['./add-drug.component.css']
 })
 export class AddDrugComponent implements OnInit {
-  constructor(private myService: AdminService, private router: Router) {
-  }
 
-  ID: any;
   Drug: DrugDto = new DrugDto("", 0);
   Issues: any;
+  treatedIssuesChecked: any[] = [];
+  conflictedIssuesChecked: any[] = [] ;
 
-
-  AddedDrug = new FormGroup({
-    id: new FormControl(""),
-    name: new FormControl(""),
-    method: new FormControl(""),
-  });
+  constructor(private myService: AdminService, private router: Router) {
+  }
 
   ngOnInit() {
     this.myService.getAllIssues().subscribe({
@@ -37,29 +32,54 @@ export class AddDrugComponent implements OnInit {
       }
     });
   }
+  onConflictedChecked(event: any, id: any) {
+    const checkbox = document.getElementById("t-" + id) as HTMLInputElement;
+    if (event.target.checked) {
+      this.conflictedIssuesChecked.push(id);
+      checkbox.disabled = true;
+    } else {
+      const index = this.conflictedIssuesChecked.indexOf(id);
+      if (index >= 0) {
+        this.conflictedIssuesChecked.splice(index, 1);
+        checkbox.disabled = false;
+      }
+    }
+  }
+
+  onTreatedCheck(event: any, id: any) {
+    const checkbox = document.getElementById("c-" + id) as HTMLInputElement;
+    if (event.target.checked) {
+      this.treatedIssuesChecked.push(id);
+      checkbox.disabled = true;
+    } else {
+      const index = this.treatedIssuesChecked.indexOf(id);
+      if (index >= 0) {
+        this.treatedIssuesChecked.splice(index, 1);
+        checkbox.disabled = false;
+      }
+    }
+  }
 
   AddDrug() {
-    console.log(this.AddedDrug.value);
-    // this.ID = uuidv4();
-
-    // this.Drug.id=this.ID;
-    this.Drug.name = this.AddedDrug.value.name!;
-    this.Drug.method = Number(this.AddedDrug.value.method!) as DrugTakingMethod;
+    this.Drug.method = Number(this.Drug.method!) as DrugTakingMethod;
+    this.Drug.conflictedIssuesIds = this.conflictedIssuesChecked;
+    this.Drug.treatedIssuesIds = this.treatedIssuesChecked;
+    console.log(this.Drug);
 
     this.myService.AddNewDrug(this.Drug).subscribe({
       next: (data) => {
       },
       error: (error) => {
-        alert("Error Happened adding new drug!")
+        alert("Error Happened adding new drug!");
+        console.log(error.message);
       },
       complete: () => {
+        alert("Drug Added Successfully!");
         this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
           this.router.navigate(['/GetAllDrugs']);
         });
       }
     });
-
-
   }
 }
 
--- src/app/Components/Admin/add-drug/drugDto.ts
+++ src/app/Components/Admin/add-drug/drugDto.ts
@@ -1,16 +1,19 @@
-import { DrugTakingMethod } from '../update-drug/drugTakingMethod.enum';
+import {DrugTakingMethod} from '../update-drug/drugTakingMethod.enum';
 
 export class DrugDto {
-  id:any;
+  id: any;
   name: string;
   method: DrugTakingMethod;
-  // treatedIssuesIds?: string[];
-  // conflictedIssuesIds?: string[];
+  treatedIssuesIds?: any;
+  conflictedIssuesIds?: any;
 
-  constructor(name: string, method: DrugTakingMethod) { //, treatedIssuesIds?: string[], conflictedIssuesIds?: string[]
+  constructor(name: string,
+              method: DrugTakingMethod,
+              treatedIssuesIds?: any,
+              conflictedIssuesIds?: any) {
     this.name = name;
     this.method = method;
-    // this.treatedIssuesIds = treatedIssuesIds;
-    // this.conflictedIssuesIds = conflictedIssuesIds;
+    this.treatedIssuesIds = treatedIssuesIds;
+    this.conflictedIssuesIds = conflictedIssuesIds;
   }
 }
