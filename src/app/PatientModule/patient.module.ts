b519f88d8a3f09f35e125c20596ef0265f596614
zeinab Mahmoud <71345608+zeynab335@users.noreply.github.com>
2023-04-24 13:19:56 +0200

    Add Global Service to get url to Connect to APi

--- src/app/PatientModule/patient.module.ts
+++ src/app/PatientModule/patient.module.ts
@@ -2,12 +2,14 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientComponent } from './patient-profile/patient-profile.component';
 import { PatientRoutingModule } from './patient-routing.module';
+import { PatientDashboardComponent } from './patient-dashboard/patient-dashboard.component';
 
 
 
 @NgModule({
   declarations: [
-    PatientComponent
+    PatientComponent,
+    PatientDashboardComponent
   ],
   imports: [
     CommonModule,
