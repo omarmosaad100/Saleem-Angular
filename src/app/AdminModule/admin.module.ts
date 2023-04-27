ecc60da4ad2ce34e7ad9565ae8af60852fc77fb6
Momostafa1997 <mohammed_ahmed97@outlook.com>
2023-04-27 05:12:16 +0200

    patient

---
+++ src/app/AdminModule/admin.module.ts
@@ -0,0 +1,54 @@
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from "@angular/core";
+import {CommonModule, DatePipe} from "@angular/common";
+import {AdminContainerComponent} from "./AdminContainer/admin-container.component";
+import {GetAllDrugsComponent} from "./get-all-drugs/get-all-drugs.component";
+import {GetDrugByIdComponent} from "./get-drug-by-id/get-drug-by-id.component";
+import {UpdateDrugComponent} from "./update-drug/update-drug.component";
+import {GetAllDoctorsComponent} from "./get-all-doctors/get-all-doctors.component";
+import {AddLicenseComponent} from "./add-license/add-license.component";
+import {AddIssueComponent} from "./add-issue/add-issue.component";
+import {DeleteDrugComponent} from "./delete-drug/delete-drug.component";
+import {AddDrugComponent} from "./add-drug/add-drug.component";
+import {AdminRoutingModule} from "./admin-routing.module";
+import {AdminApiService} from "./AdminServices/admin-api-calls/admin.api.service";
+import {NgbPaginationModule, NgbTypeaheadModule} from "@ng-bootstrap/ng-bootstrap";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AdminLayoutComponent} from './admin-layout/admin-layout.component';
+import {LayoutModule} from "../LayoutModule/layout.module";
+import {AdminLayoutModule} from "./admin-layout/admin-layout.module";
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+
+
+@NgModule({
+  declarations: [
+    DeleteDrugComponent,
+    AddDrugComponent,
+    AdminContainerComponent,
+    GetAllDrugsComponent,
+    GetDrugByIdComponent,
+    UpdateDrugComponent,
+    GetAllDoctorsComponent,
+    AddLicenseComponent,
+    AddIssueComponent,
+    AdminLayoutComponent,
+    AdminLoginComponent,
+  ],
+  imports: [
+    AdminRoutingModule,
+    CommonModule,
+    NgbTypeaheadModule,
+    NgbPaginationModule,
+    ReactiveFormsModule,
+    FormsModule,
+    LayoutModule,
+    AdminLayoutModule
+  ],
+  providers: [DatePipe, AdminApiService, {
+    provide: 'text/css', useValue: 'text/css'
+  }],
+
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
+})
+export class AdminModule {
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AuthContainerComponent} from './Components/Authentication/AuthContainer/AuthContainer.component';
-//import { HomePageComponent } from './Components/HomePage/HomePage/HomePage.component';
-import {AddDrugComponent} from './AdminModule/add-drug/add-drug.component';
-import {DeleteDrugComponent} from './AdminModule/delete-drug/delete-drug.component';
-import {GetAllDrugsComponent} from './AdminModule/get-all-drugs/get-all-drugs.component';
-import {GetDrugByIdComponent} from './AdminModule/get-drug-by-id/get-drug-by-id.component';
-import {UpdateDrugComponent} from './AdminModule/update-drug/update-drug.component';
-import {GetAllDoctorsComponent} from './AdminModule/get-all-doctors/get-all-doctors.component';
-import {AddLicenseComponent} from './AdminModule/add-license/add-license.component';
-import {AddIssueComponent} from './AdminModule/add-issue/add-issue.component';
-
 import {ErrorPageComponent} from './LayoutModule/Components/error-page/error-page.component';
 import {HomePageComponent} from './HomePageModule/home-page/home-page.component';
 import {LayoutComponent} from './LayoutModule/layout.component';
+<<<<<<< HEAD
 import {PatientAppointmentsComponent} from './PatientModule/patient-appointments/patient-appointments.component';
 import {PatientDrugsComponent} from './PatientModule/patient-Drugs/Patient-Drugs.component';
 import {PatientIllnessesComponent} from './PatientModule/patient-illnesses/patient-illnesses.component';
 import {AdminContainerComponent} from "./AdminModule/AdminContainer/admin-container.component";
+=======
+import {AdminLayoutComponent} from "./AdminModule/admin-layout/admin-layout.component";
+>>>>>>> 006b4cf8618203a889207d6ccb52ba00ec710e0b
 
 
 const routes: Routes = [
@@ -28,37 +22,26 @@ const routes: Routes = [
         path: ""
         , loadChildren: () => import('./DoctorModule/doctor.module')
           .then(m => m.DoctorModule)
-      },
-      {
-        path: "Admin", component: AdminContainerComponent, children: [
-          {path: "AddDrug", component: AddDrugComponent},
-          {path: "AddLicense", component: AddLicenseComponent},
-          {path: "AddIssue", component: AddIssueComponent},
-          {path: "DeleteDrug/:id", component: AddLicenseComponent},
-          {path: "GetAllDrugs", component: GetAllDrugsComponent},
-          {path: "GetAllDoctors", component: GetAllDoctorsComponent},
-          {path: "GetDrug/:id", component: GetDrugByIdComponent},
-          {path: "UpdateDrug/:id", component: UpdateDrugComponent},
-        ]
-      },
-    ]
+      }]
   },
-
   {
     path: "patient"
     , loadChildren: () => import('./PatientModule/patient.module')
       .then(p => p.PatientModule)
   },
+  {
+    path: "", component: AdminLayoutComponent, children: [
+      {
+        path: "Admin"
+        , loadChildren: () => import('./AdminModule/admin.module')
+          .then(a => a.AdminModule)
+      }
+    ]
 
+  },
   {path: "Home", redirectTo: ""},
-
-
   {path: "Login", component: AuthContainerComponent},
   {path: "Register", component: AuthContainerComponent},
-
-  //admin components
-
-
   {path: "**", component: ErrorPageComponent}];
 
 @NgModule({
